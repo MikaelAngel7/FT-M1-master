6679638c8977e77498d9258f1eb71ea0c8016dd0
MikaelAngel7 <angel.chavez.micaela@gamil.com>
2023-02-22 19:57:13 -0300

    homework01

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,8 +1,20 @@
 'use strict';
 
-function BinarioADecimal(num) {}
+function BinarioADecimal(num) {
+   let sum = 0;
+   for (let i = 0; i < num.length; i++) {
+      sum += num[i] * 2 ** (num.length - 1 - i);
+   }
+   return sum;
+}
+console.log(BinarioADecimal("1011"));
 
-function DecimalABinario(num) {}
+function DecimalABinario(num) {
+   if (num > 0) {
+      return DecimalABinario(parseInt(num / 2) ) + (num % 2)
+   };
+   return "";
+}
 
 module.exports = {
    BinarioADecimal,
